06523c4624f53addd64f3332e0b3a3d7fc5e29da
lopezeliasg <lopezeliasga@gmail.com>
2021-09-03 03:57:42 -0300

    Update settings.py

--- ProyectoInfo/settings.py
+++ ProyectoInfo/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
