bb38233bb2c93c00b49c5e6879f5f3bc0da48536
lopezeliasg <lopezeliasga@gmail.com>
2021-09-03 03:31:08 -0300

    Update settings.py

--- ProyectoInfo/settings.py
+++ ProyectoInfo/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
